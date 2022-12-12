591eef29ab9f942905103760d979ade23537032e
hyonna12 <hyonna01@gmail.com>
2022-12-12 14:36:21 +0900

    카테고리추가 ajax 통신완료

--- src/main/java/site/hobbyup/class_final_back/dto/category/CategoryReqDto.java
+++ src/main/java/site/hobbyup/class_final_back/dto/category/CategoryReqDto.java
@@ -1,11 +1,15 @@
 package site.hobbyup.class_final_back.dto.category;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import site.hobbyup.class_final_back.domain.category.Category;
 
 public class CategoryReqDto {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
     @Setter
     @Getter
     public static class CategorySaveReqDto {
